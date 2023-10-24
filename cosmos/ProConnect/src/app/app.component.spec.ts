ee179d62c6645e85d1fa0636f088ad3dba13209e
Gabriel Sichoski <104863390+GabrielSichoski@users.noreply.github.com>
2023-10-24 18:16:17 -0300

    Add files via upload

--- cosmos/ProConnect/src/app/app.component.spec.ts
+++ cosmos/ProConnect/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ProConnect'`, () => {
+  it(`should have as title 'Proconnect'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ProConnect');
+    expect(app.title).toEqual('Proconnect');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ProConnect app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Proconnect app is running!');
   });
 });
--- cosmos/ProConnect/src/app/app.module.ts
+++ cosmos/ProConnect/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { PagInicialComponent } from './pag-inicial/pag-inicial.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PagInicialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
