6aef30a9286f4fd51e8424289189e7ec6a9ba81d
Gabriel Sichoski <104863390+GabrielSichoski@users.noreply.github.com>
2023-10-29 15:47:30 -0300

    Add files via upload

--- cosmos/ProConnect/src/app/login/login.component.ts
+++ cosmos/ProConnect/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,27 +8,44 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'] 
 })
 export class LoginComponent {
-  email: String = "";
-  senha: String = "";
+  email: string = "";
+  senha: string = "";
 
   constructor(private http: HttpClient, private router: Router){} 
   ngOnInit(): void{}
 
   verificarUsuario(){
-    this.http.post('https://64e6a1f309e64530d1801dd3.mockapi.io/api/v1/Nome', { email: this.email, senha: this.senha })
-      .subscribe((response: any) => {
+    window.alert('Verificando dados do usuário...');
+
+    console.log('Email:', this.email);
+    console.log('Senha:', this.senha);
+
+    // Verifica a senha com a expressão regular
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9!@#\$%\^&\*\(\)_\+\-=\[\]\{\};:'",<>\./?\\|]).{6,}$/;
+    if (!regex.test(this.senha)) {
+      window.alert('Senha fora dos padrões');
+      return;
+    }
+
+    // URL do servidor
+    const url = `https://64e6a1f309e64530d1801dd3.mockapi.io/api/v1/Nome?email=${this.email}&senha=${this.senha}`;
+
+    // Faz uma solicitação GET para o servidor
+    this.http.get(url).subscribe(
+      (response: any) => {
         console.log(response);
-        if (response.LoginOk){
+        if (response.LoginOk ) {
+          window.alert('Login efetuado com sucesso')
           this.router.navigate(['./pag-inicial']);
+        } else {
+          window.alert('Email ou Senha incorretos')
         }
-        else { 
-
-        }
-  },
-  error => {
-    console.error(error);
-  });
+      },
+      error => {
+        console.error(error);
+        window.alert('Ocorreu um erro ao efetuar o login')
+      }
+    );
+  }
 }
-}
-
-//const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9!@#\$%\^&\*\(\)_\+\-=\[\]\{\};:'",<>\./?\\|]).{6,}$/;//
+// Email: test@gmail.com  Senha: Senha#
